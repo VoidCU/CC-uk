d699b56b64e1139383417b3209808a3e34ea69fa
Bot69 <rupeshacharya2000@gmail.com>
2024-03-14 00:39:51 +0545

    remove loader

--- js/logo-loader.js
+++ js/logo-loader.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     // Wait for the DOM to be fully loaded
 
     // Stop zoom-in and zoom-out animation after 2 seconds
-    setTimeout(function() {
-        document.getElementById("logo-container").style.animation = "none";
-    }, 2000);
+    // setTimeout(function() {
+    //     document.getElementById("logo-container").style.animation = "none";
+    // }, 1);
 
     // Display the logo for 4 seconds before fading out
     setTimeout(function() {
         document.getElementById("logo-container").style.opacity = 0;
         document.getElementById("content-container").style.opacity = 1;
-    }, 2000);
+    }, 1);
 });
